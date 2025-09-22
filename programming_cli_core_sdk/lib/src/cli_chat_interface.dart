08aa0817dd25fed31676b762fcc379feef8f41e9
Igor <igor_midev@outlook.com>
2025-09-21 23:11:12 -0300

    Implements message handling with schema support
    Enhances message sending and streaming methods by integrating
    schema handling through structured data completers.
    
    Refines sendMessage and streamResponse to utilize temporary
    file management, enabling JSON responses according to a
    specified schema. Includes schema validation tests to
    ensure adherence to the defined structure.
    
    Removes obsolete code related to schema file handling for
    improved clarity and performance.

--- programming_cli_core_sdk/lib/src/cli_chat_interface.dart
+++ programming_cli_core_sdk/lib/src/cli_chat_interface.dart
@@ -42,13 +42,51 @@ abstract class CliChatInterface<T extends CliChatOptions> {
   });
 
   Directory get baseDir;
-  Future<String> sendMessage(List<PromptContent> contents);
-  Stream<String> streamResponse(List<PromptContent> contents);
+  Future<String> sendMessage(List<PromptContent> contents) async {
+    final (
+      :Stream<String> llmMessage,
+      :Completer<Map<String, dynamic>?> structuredSchemaData,
+    ) = _handleTemporaryFilesWrapper(
+      promptsOfCurrentMessage: contents,
+      schema: null,
+    );
+
+    return await llmMessage.join();
+  }
+
+  Stream<String> streamResponse(List<PromptContent> contents) {
+    final (
+      :Stream<String> llmMessage,
+      :Completer<Map<String, dynamic>?> structuredSchemaData,
+    ) = _handleTemporaryFilesWrapper(
+      promptsOfCurrentMessage: contents,
+      schema: null,
+    );
+
+    return llmMessage;
+  }
+
   Future<({String llmMessage, Map<String, dynamic> structuredSchemaData})>
   sendMessageWithSchema({
     required List<PromptContent> messages,
     required SchemaObject schema,
-  });
+  }) async {
+    final (
+      :Stream<String> llmMessage,
+      :Completer<Map<String, dynamic>?> structuredSchemaData,
+    ) = _handleTemporaryFilesWrapper(
+      promptsOfCurrentMessage: messages,
+      schema: schema,
+    );
+
+    final schemaData = await structuredSchemaData.future;
+    final fullMessage = await llmMessage.join();
+    if (schemaData == null) {
+      throw Exception('Schema data is null');
+    }
+    return (llmMessage: fullMessage, structuredSchemaData: schemaData);
+  }
+
   ({
     Stream<String> llmMessage,
     Completer<Map<String, dynamic>> structuredSchemaData,
@@ -56,84 +94,43 @@ abstract class CliChatInterface<T extends CliChatOptions> {
   streamResponseWithSchema({
     required List<PromptContent> messages,
     required SchemaObject schema,
-  });
+  }) {
+    final (
+      :Stream<String> llmMessage,
+      :Completer<Map<String, dynamic>?> structuredSchemaData,
+    ) = _handleTemporaryFilesWrapper(
+      promptsOfCurrentMessage: messages,
+      schema: schema,
+    );
+
+    final completer = Completer<Map<String, dynamic>>();
+    structuredSchemaData.future
+        .then((value) {
+          if (value == null) {
+            completer.completeError(Exception('Schema data is null'));
+          } else {
+            completer.complete(value);
+          }
+        })
+        .catchError((error, stackTrace) {
+          if (!completer.isCompleted) {
+            completer.completeError(error, stackTrace);
+          }
+        });
+
+    return (llmMessage: llmMessage, structuredSchemaData: completer);
+  }
 
   String get schemaResponseFilePath =>
       '${baseDir.path}/schema_response_id-$_chatNanoId.json';
   String get schemaTestFilePath =>
       '${baseDir.path}/is_schema_id-${_chatNanoId}_correct_test.dart';
 
-  Future<void> _setupSchemaFiles(SchemaObject schema) async {
-    // First, lets create the json file where the AI will write the response in the json schema format
-    final schemaFile = await File(schemaResponseFilePath).create();
-    await schemaFile.writeAsString('{}');
-    // Now, create the schema instructions file
-    final schemaTestFolder = await File(schemaTestFilePath).create();
-
-    final String testContent =
-        '''import 'dart:convert';
-import 'dart:io';
-import 'package:test/test.dart';
-
-void main() {
-  test('Is correct schema', () async {
-    final currDir = Directory.current;
-    final schemaFile = File('$schemaResponseFilePath');
-    final schemaContent = await schemaFile.readAsString();
-    final Map<String, dynamic> schemaJson = jsonDecode(schemaContent);
-    final bool isValidSchema = schema.validateIdJsonFollowsSchemaStructure(
-      schemaJson,
-    );
-    expect(isValidSchema, true);
-  });
-}
-
-final SchemaPropertyStructuredObjectWithDefinedProperties schema = ${schema.toDartClassDeclaration} as SchemaPropertyStructuredObjectWithDefinedProperties;''';
-
-    await schemaTestFolder.writeAsString(testContent);
-  }
-
-  Future<void> _removeSchemaFiles() async {
-    try {
-      await File(schemaResponseFilePath).delete();
-      await File(schemaTestFilePath).delete();
-    } catch (_) {
-      // Ignore errors during cleanup
-    }
-  }
-
-  Future<String> _generateSchemaPrompt({required SchemaObject schema}) async {
-    return '''----------- SCHEMA INSTRUCTIONS [START] -----------
-    
-I wan't the output in a json format. 
-I wan't you to write the output in the json file located at: $filePreffix$schemaResponseFilePath
-
-But I don't wan't the json in any random format.
-It should be in a specific schema that I will provide you below.
-You MUST follow the schema EXACTLY as I provide you.
-If you don't follow the schema, I will not be able to parse it.
-Because of that, since I need 100% of certainty, I created a dart test code that will validate if you followed the schema or not.
-The test code is located at: $filePreffix$schemaTestFilePath
-
-IMPORTANT: Open the test file - You will see that it reads the file at $filePreffix$schemaResponseFilePath and validates if it follows the schema or not.
-So, you MUST follow the schema EXACTLY as I provide you below or the test will fail.
-
-If the test fails, you can be sure you did not follow the schema - so see the errors logs of the test and fix it.
-DO NOT change anything in this test file, it is perfect as it is - just focus on following the schema and it will naturally pass the test.
-
-The schema expected is:
-```json
-${schema.toString()}
-```
-
------------ SCHEMA INSTRUCTIONS [END] -----------''';
-  }
-
   ({
     Stream<String> llmMessage,
     Completer<Map<String, dynamic>?> structuredSchemaData,
   })
-  handleTemporaryFilesWrapper({
+  _handleTemporaryFilesWrapper({
     required List<PromptContent> promptsOfCurrentMessage,
     required SchemaObject? schema,
   }) {
@@ -186,21 +183,21 @@ $testErrorMessage''',
               streamSink: controller.sink,
             );
 
-            final stillHasErrorMessage = await _isSchemaResponseValid();
-            if (stillHasErrorMessage != null) {
-              // If it still has error, we throw
-              throw Exception(
-                'Schema validation failed again. Last error: $stillHasErrorMessage',
-              );
+            final errorMessage = await _isSchemaResponseValid();
+            final stillFailed = errorMessage != null;
+            if (stillFailed) {
+              controller.close();
+              responseCompleter.completeError(errorMessage);
+              return;
             }
           }
 
           final fileContent = await File(schemaResponseFilePath).readAsString();
           final json = jsonDecode(fileContent) as Map<String, dynamic>;
-          responseCompleter.complete(json);
-
           await _cleanupTemporaryFiles();
           await _removeSchemaFiles();
+          controller.close();
+          responseCompleter.complete(json);
         } catch (error, stackTrace) {
           await _cleanupTemporaryFiles();
           if (schema != null) await _removeSchemaFiles();
@@ -217,6 +214,72 @@ $testErrorMessage''',
       llmMessage: controller.stream,
       structuredSchemaData: responseCompleter,
     );
+  }
+
+  Future<String> _generateSchemaPrompt({required SchemaObject schema}) async {
+    return '''----------- SCHEMA INSTRUCTIONS [START] -----------
+    
+I wan't the output in a json format. 
+I wan't you to write the output in the json file located at: $filePreffix$schemaResponseFilePath
+
+But I don't wan't the json in any random format.
+It should be in a specific schema that I will provide you below.
+You MUST follow the schema EXACTLY as I provide you.
+If you don't follow the schema, I will not be able to parse it.
+Because of that, since I need 100% of certainty, I created a dart test code that will validate if you followed the schema or not.
+The test code is located at: $filePreffix$schemaTestFilePath
+
+IMPORTANT: Open the test file - You will see that it reads the file at $filePreffix$schemaResponseFilePath and validates if it follows the schema or not.
+So, you MUST follow the schema EXACTLY as I provide you below or the test will fail.
+
+If the test fails, you can be sure you did not follow the schema - so see the errors logs of the test and fix it.
+DO NOT change anything in this test file, it is perfect as it is - just focus on following the schema and it will naturally pass the test.
+
+The schema expected is:
+```json
+${schema.toString()}
+```
+
+----------- SCHEMA INSTRUCTIONS [END] -----------''';
+  }
+
+  Future<void> _setupSchemaFiles(SchemaObject schema) async {
+    // First, lets create the json file where the AI will write the response in the json schema format
+    final schemaFile = await File(schemaResponseFilePath).create();
+    await schemaFile.writeAsString('{}');
+    // Now, create the schema instructions file
+    final schemaTestFolder = await File(schemaTestFilePath).create();
+
+    final String testContent =
+        '''import 'dart:convert';
+import 'dart:io';
+import 'package:test/test.dart';
+
+void main() {
+  test('Is correct schema', () async {
+    final currDir = Directory.current;
+    final schemaFile = File('$schemaResponseFilePath');
+    final schemaContent = await schemaFile.readAsString();
+    final Map<String, dynamic> schemaJson = jsonDecode(schemaContent);
+    final bool isValidSchema = schema.validateIdJsonFollowsSchemaStructure(
+      schemaJson,
+    );
+    expect(isValidSchema, true);
+  });
+}
+
+final SchemaPropertyStructuredObjectWithDefinedProperties schema = ${schema.toDartClassDeclaration} as SchemaPropertyStructuredObjectWithDefinedProperties;''';
+
+    await schemaTestFolder.writeAsString(testContent);
+  }
+
+  Future<void> _removeSchemaFiles() async {
+    try {
+      await File(schemaResponseFilePath).delete();
+      await File(schemaTestFilePath).delete();
+    } catch (_) {
+      // Ignore errors during cleanup
+    }
   }
 
   Future<void> _saveNewPromptContents(
