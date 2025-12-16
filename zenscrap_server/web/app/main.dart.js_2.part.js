0f3189b54662f1ac71b7a9062c21f56745a9a47e
Igor <igor_midev@outlook.com>
2025-12-16 17:31:51 +0900

    wasm fix

--- zenscrap_server/web/app/main.dart.js_2.part.js
+++ zenscrap_server/web/app/main.dart.js_2.part.js
@@ -1,8 +1,8 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={f8:function f8(){}}
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={fa:function fa(){}}
 B=c[0]
 A=a.updateHolder(c[9],A)
-A.f8.prototype={}
+A.fa.prototype={}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.f8,B.D)})()};
-(a=>{a["VGmKCOvb/d2CeUvzKCaFlh6qduc="]=a.current})($__dart_deferred_initializers__);
+y(A.fa,B.E)})()};
+(a=>{a["Nk5aDX8EfWbtPR4BKcpK/51RJKI="]=a.current})($__dart_deferred_initializers__);
