ebf6ce08e8e70b0c09a14801a2d379db48d0b8d5
Igor <igor_midev@outlook.com>
2025-12-19 23:56:52 +0900

    Version update

--- zenscrap_server/web/app/main.dart.js_2.part.js
+++ zenscrap_server/web/app/main.dart.js_2.part.js
@@ -1,8 +1,8 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={fa:function fa(){}}
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={f1:function f1(){}}
 B=c[0]
 A=a.updateHolder(c[9],A)
-A.fa.prototype={}
+A.f1.prototype={}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.fa,B.E)})()};
-(a=>{a["Nk5aDX8EfWbtPR4BKcpK/51RJKI="]=a.current})($__dart_deferred_initializers__);
+y(A.f1,B.B)})()};
+(a=>{a["g+r4d8+BBN0oRYqeRUtECAg9ZY0="]=a.current})($__dart_deferred_initializers__);
